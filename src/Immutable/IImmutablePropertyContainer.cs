52d82baf085fe28858991bc6bc46aca0a3229523
Exeteres <exeteres@yandex.ru>
2022-01-05 22:36:22 +0700

    feat: Add IImmutablePropertyContainer interface

---
+++ src/Immutable/IImmutablePropertyContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace Kantaiko.Properties.Immutable;
+
+public interface IImmutablePropertyContainer : IReadOnlyPropertyContainer
+{
+    new IImmutablePropertyCollection Properties { get; }
+
+    IReadOnlyPropertyCollection IReadOnlyPropertyContainer.Properties => Properties;
+}
